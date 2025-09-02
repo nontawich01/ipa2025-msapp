5d647d41f88cdca4eab5c0d0449531f191e46547
Nontawich Tungkun <66070276@kmitl.ac.th>
2025-09-02 16:50:14 +0700

    pep888

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -11,11 +11,7 @@ def produce(host, body):
         queue="router_jobs", exchange="jobs", routing_key="check_interfaces"
     )
 
-    channel.basic_publish(
-        exchange="jobs",
-        routing_key="check_interfaces",
-        body=body
-    )
+    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
 
     connection.close()
 
